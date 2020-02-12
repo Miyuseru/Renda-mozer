6759825e79b62e06591cdcc1f57c64b7757f5e9f
miyuu kuwana <miyuu13k@gmail.com>
2020-02-12 14:32:09 +0900

    Renda

--- app/src/main/java/app/miyuseru/renda/MainActivity.kt
+++ app/src/main/java/app/miyuseru/renda/MainActivity.kt
@@ -1,22 +1,56 @@
 package app.miyuseru.renda
 
+import android.app.PendingIntent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var second = 10
 
-    var tapCount =0
+    var tapCount = 0
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    var timer: CountDownTimer = object : CountDownTimer(10000, 1000) {
+        override fun onFinish() {
 
+            startButton.isVisible = true
+            tapButton.setBackgroundResource(R.drawable.background_rounded_circle_gray)
+            second = 10
+            tapCount = 0
+        }
 
-        tapButton.setOnClickListener{
-            tapCount = tapCount+1
-            countText.text = tapCount.toString()
-
+        override fun onTick() {
+            tapButton.setBackgroundResource(R.drawable.background_ronded_circle)
+            second = second - 1
+            secondText.text = second.toString()
         }
     }
-}
+
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+
+            startButton.setOnClickListener {
+                countText.text = tapCount.toString()
+                startButton.isVisible = false
+                timer.start()
+            }
+
+
+            tapButton.setOnClickListener {
+
+                if (second < 10) {
+                    tapCount = tapCount + 1
+                    countText.text = tapCount.toString()
+
+                }
+
+
+            }
+        }
+    }
+
+
